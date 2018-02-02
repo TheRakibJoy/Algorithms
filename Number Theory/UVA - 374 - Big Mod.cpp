0d9a0fc1296ec8f7633580f180fff1f073b6c985
Md. Iqbal Hossain <32066256+Bappy38@users.noreply.github.com>
2018-02-02 12:38:42 +0600

    Add files via upload

---
+++ Number Theory/UVA - 374 - Big Mod.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+
+#define ll                      long long int
+#define ull                     unsigned long long int
+#define pii                     pair<int,int>
+#define pll                     pair<ll,ll>
+#define sc                      scanf
+#define scin(x)                 sc("%d",&(x))
+#define scln(x)                 sc("%lld",&(x))
+#define pf                      printf
+#define ms(a,b)                 memset(a,b,sizeof(a))
+#define pb(a)                   push_back(a)
+#define mp                      make_pair
+#define db                      double
+#define EPS                     10E-10
+#define ff                      first
+#define ss                      second
+#define sqr(x)                  (x)*(x)
+#define vi                      vector<int>
+#define vl                      vector<ll>
+#define vii                     vector<vector<int> >
+#define vll                     vector<vector<ll> >
+#define DBG                     pf("HI\n")
+#define MOD                     1000000007
+#define CIN                     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define RUN_CASE(t,T)           for(__typeof(t) t=1;t<=T;t++)
+#define CASE(t)                 printf("Case %d: ",t)
+#define CASEl(t)                printf("Case %d:\n",t)
+#define intlimit                2147483648
+#define longlimit               9223372036854775808
+#define infinity                (1<<28)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                ((a)*(b))/gcd(a,b)
+#define mx                      123456789
+#define PI                      2*acos(0.0)
+#define rep(i,a,b)              for(__typeof(i) i=a;i<=b;i++)
+#define rev(i,a,b)              for(__typeof(i) i=a;i>=b;i--)
+
+using namespace std;
+
+///------------------Graph Moves-------------------
+/// const int fx[] = {+1,-1,+0,+0};
+/// const int fy[] = {+0,+0,+1,-1};
+/// const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+/// const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///King's move
+/// const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///Knight's move
+/// const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///Knight's move
+
+ll BigMod(ll a,ll b,ll m)
+{
+    if(b == 0)
+        return 1%m;
+    else if(b%2 == 0)
+    {
+        ll temp=BigMod(a,b/2,m);
+        return ((temp%m)*(temp%m))%m;
+    }
+    else
+    {
+        return ((a%m)*BigMod(a,b-1,m)%m)%m;
+    }
+}
+int main()
+{
+    ll a,b,m,res;
+    while(scanf("%lld %lld %lld",&a,&b,&m) != EOF)
+    {
+        res=BigMod(a,b,m);
+        pf("%lld\n",res);
+    }
+    return 0;
+}
