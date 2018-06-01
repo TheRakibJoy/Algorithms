c18ee3e291bbdc40530eb32faec9a3fc88ce91d1
Md. Iqbal Hossain <32066256+Bappy38@users.noreply.github.com>
2018-06-01 20:19:54 +0600

    Add files via upload

---
+++ Dynamic Programming/More efficient coin change.cpp
@@ -0,0 +1,89 @@
+/**
+Bismillahir Rahmanir Rahim
+MD. Iqbal Hossain Bappy
+Dept. of CSE, CoU
+**/
+
+#include<bits/stdc++.h>
+
+#define perfect                 int main()
+#define ll                      long long int
+#define ull                     unsigned long long int
+#define pii                     pair<int,int>
+#define pll                     pair<ll,ll>
+#define sc                      scanf
+#define scin(x)                 sc("%d",&(x))
+#define scln(x)                 sc("%lld",&(x))
+#define pf                      printf
+#define ms(a,b)                 memset(a,b,sizeof(a))
+#define pb(a)                   push_back(a)
+#define mp                      make_pair
+#define db                      double
+#define EPS                     10E-10
+#define ff                      first
+#define ss                      second
+#define sqr(x)                  (x)*(x)
+#define vi                      vector<int>
+#define vl                      vector<ll>
+#define vii                     vector<vector<int> >
+#define vll                     vector<vector<ll> >
+#define DBG                     pf("HI\n")
+#define MOD                     1000000007
+#define CIN                     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define RUN_CASE(t,T)           for(__typeof(t) t=1;t<=T;t++)
+#define CASE(t)                 printf("Case %d: ",t)
+#define CASEl(t)                printf("Case %d:\n",t)
+#define intlimit                2147483647;
+#define longlimit               9223372036854775808
+#define infinity                (1<<28)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                ((a)*(b))/gcd(a,b)
+#define mxx                     123456789
+#define PI                      2*acos(0.0)
+#define rep(i,a,b)              for(__typeof(i) i=a;i<=b;i++)
+#define rev(i,a,b)              for(__typeof(i) i=a;i>=b;i--)
+
+using namespace std;
+
+///------------------Graph Moves-------------------
+/// const int fx[] = {+1,-1,+0,+0};
+/// const int fy[] = {+0,+0,+1,-1};
+/// const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+/// const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///King's move
+/// const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///Knight's move
+/// const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///Knight's move
+
+int coin[5]={50,25,10,5,1};
+int make;
+int dp[6][8000];
+int CoinChange(int i,int amount)
+{
+    if(i >= 5)
+    {
+        if(amount == 0)
+            return 1;
+        else
+            return 0;
+    }
+    if(dp[i][amount] != -1)
+        return dp[i][amount];
+    int ret1=0,ret2=0;
+    if(amount-coin[i] >= 0)
+        ret1=CoinChange(i,amount-coin[i]);
+    ret2=CoinChange(i+1,amount);
+    return dp[i][amount] = ret1+ret2;/** number of ways to make the amount**/
+}
+perfect
+{
+    int x;
+    ms(dp,-1);
+    while(scin(make) != EOF)
+    {
+        /**No need to clear the dp array frequently,but the values of coin are not changeable
+        in this technique**/
+        x=CoinChange(0,make);
+        pf("%d\n",x);
+    }
+    return 0;
+}
+
