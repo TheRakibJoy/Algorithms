d742d7130b366fb49bfe8ccb5319f2d3c0c36453
Md. Iqbal Hossain <32066256+Bappy38@users.noreply.github.com>
2018-10-06 15:56:04 +0600

    Add files via upload

---
+++ Dynamic Programming/UVA-10664 - Luggage.cpp
@@ -0,0 +1,111 @@
+#include<bits/stdc++.h>
+
+#define perfect                 int main()
+#define ll                      long long int
+#define ull                     unsigned long long int
+#define pii                     pair<int,int>
+#define pll                     pair<ll,ll>
+#define sc                      scanf
+#define scin(x)                 sc("%d",&(x))
+#define scin2(x,y)                sc("%d %d",&(x),&(y))
+#define scln(x)                 sc("%lld",&(x))
+#define scln2(x,y)                sc("%lld %lld",&(x),&(y))
+#define pf                      printf
+#define ms(a,b)                 memset(a,b,sizeof(a))
+#define pb(a)                   push_back(a)
+#define mp                      make_pair
+#define db                      double
+#define EPS                     10E-10
+#define ff                      first
+#define ss                      second
+#define sqr(x)                  (x)*(x)
+#define vi                      vector<int>
+#define vl                      vector<ll>
+#define vii                     vector<vector<int> >
+#define vll                     vector<vector<ll> >
+#define DBG                     pf("HI\n")
+//#define MOD                     100000007
+#define CIN                     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define RUN_CASE(t,T)           for(__typeof(t) t=1;t<=T;t++)
+#define CASE(t)                 printf("Case %d: ",t)
+#define CASEl(t)                printf("Case %d:\n",t)
+#define intlimit                214748369
+#define longlimit               922337203685477580
+#define infinity                (1<<28)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                ((a)*(b))/gcd(a,b)
+#define mxx                     123456789
+#define PI                      2*acos(0.0)
+#define rep(i,a,b)              for(__typeof(i) i=a;i<=b;i++)
+#define rev(i,a,b)              for(__typeof(i) i=a;i>=b;i--)
+
+using namespace std;
+/** toint, tostring, BigMod, sieve, Primefactorize **/
+//ll toint(string s){ll n=0,k=1;for(int i=s.size()-1;i>=0;i--){n += ((s[i]-'0')*k);k*=10;}return n;}
+//string tostring(ll n){string s="";ll x;while(n > 0){x=n%10;s +=(char)(x+'0');n/=10;}reverse(s.begin(),s.end());return s;}
+//ll BigMod(ll a,ll b,ll m){if(b == 0)return 1%m;else if(b%2 == 0){ll temp=BigMod(a,b/2,m);return ((temp%m)*(temp%m))%m;}else{return ((a%m)*BigMod(a,b-1,m)%m)%m;}}
+
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+//template<typename T> using orderset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+//orderset<int> s ; //orderset<int>::iterator it ;
+//orderset<int> X; //X.insert(1); //X.insert(2); //X.insert(4); //X.insert(8); //X.insert(16);
+//cout<<*X.find_by_order(1)<<endl; // 2 //cout<<*X.find_by_order(2)<<endl; // 4 //cout<<*X.find_by_order(4)<<endl; // 16 //cout<<(end(X)==X.find_by_order(6))<<endl; // true
+//cout<<X.order_of_key(-5)<<endl;  // 0 //cout<<X.order_of_key(1)<<endl;   // 0 //cout<<X.order_of_key(3)<<endl;   // 2 //cout<<X.order_of_key(4)<<endl;   // 2 //cout<<X.order_of_key(400)<<endl; // 5
+
+vector<int>weight;
+int dp[21][205];
+
+int fun(int i,int n,int tot)
+{
+    if(i >= n)
+    {
+        if(tot == 0)
+            return 1;
+        else
+            return 0;
+    }
+    if(dp[i][tot] != -1)
+        return dp[i][tot];
+    int ret1=0,ret2=0;
+    if(tot-weight[i] >= 0)
+        ret1=fun(i+1,n,tot-weight[i]);
+    ret2=fun(i+1,n,tot);
+    return dp[i][tot]=ret1|ret2;
+}
+int main()
+{
+    int t,T,n,tot,half,temp,ans;
+    scin(T);
+    getchar();
+    RUN_CASE(t,T)
+    {
+        n=0;
+        tot=0;
+        weight.clear();
+        ms(dp,-1);
+        string s;
+        getline(cin,s);
+        stringstream ss;
+        ss<<s;
+        while(ss>>temp)
+        {
+            weight.pb(temp);
+            tot+=temp;
+            n++;
+        }
+        if(tot%2 == 0)
+        {
+            half=tot/2;
+            ans=fun(0,n,half);
+            if(ans == 1)
+                pf("YES\n");
+            else
+                pf("NO\n");
+        }
+        else
+            pf("NO\n");
+    }
+    return 0;
+}
+
