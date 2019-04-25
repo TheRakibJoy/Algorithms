1410df38990839ac7c3c5b76dffb4482e7f322f6
Md. Iqbal Hossain <32066256+Bappy38@users.noreply.github.com>
2019-04-26 01:30:02 +0600

    Add files via upload

---
+++ Graph Theory/Minimum Spanning Tree/LOJ - 1040 - Donation with MST.cpp
@@ -0,0 +1,148 @@
+#include<bits/stdc++.h>
+
+#define Input                   freopen("in.txt","r",stdin)
+#define Output                  freopen("out.txt","w",stdout)
+#define ll                      long long int
+#define ull                     unsigned long long int
+#define pii                     pair<int,int>
+#define pll                     pair<ll,ll>
+#define sc                      scanf
+#define scin(x)                 sc("%d",&(x))
+#define scin2(x,y)                sc("%d %d",&(x),&(y))
+#define scln(x)                 sc("%lld",&(x))
+#define scln2(x,y)                sc("%lld %lld",&(x),&(y))
+#define pf                      printf
+#define ms(a,b)                 memset(a,b,sizeof(a))
+#define pb(a)                   push_back(a)
+#define mp                      make_pair
+#define db                      double
+#define EPS                     10E-10
+#define ff                      first
+#define ss                      second
+#define sqr(x)                  (x)*(x)
+#define vi                      vector<int>
+#define vl                      vector<ll>
+#define vii                     vector<vector<int> >
+#define vll                     vector<vector<ll> >
+#define DBG                     pf("HI\n")
+#define MOD                     1000000007
+#define CIN                     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define RUN_CASE(t,T)           for(__typeof(t) t=1;t<=T;t++)
+#define CASE(t)                 printf("Case %d: ",t)
+#define CASEl(t)                printf("Case %d:\n",t)
+#define intlimit                214748369
+#define longlimit               92233720368547758
+#define infinity                (1<<28)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                ((a)*(b))/gcd(a,b)
+#define mxx                     123456789
+#define PI                      2*acos(0.0)
+#define rep(i,a,b)              for(__typeof(i) i=a;i<=b;i++)
+#define rev(i,a,b)              for(__typeof(i) i=a;i>=b;i--)
+
+using namespace std;
+/**
+Bit manipulation
+**/
+/** toint, tostring, BigMod, Power , sieve, Primefactorize ,frequency in n!**/
+//ll toint(string s){ll n=0,k=1;for(int i=s.size()-1;i>=0;i--){n += ((s[i]-'0')*k);k*=10;}return n;}
+//ll BigMod(ll a,ll b,ll m){if(b == 0)return 1%m;else if(b%2 == 0){ll temp=BigMod(a,b/2,m);return ((temp%m)*(temp%m))%m;}else{return ((a%m)*BigMod(a,b-1,m)%m)%m;}}
+//ll frequency(ll n,ll factor)/** Frequency of a factor in n! **/{ll cnt=0;while(n){cnt += (n/factor);n /= factor;}return cnt;}
+//ll power(ll a,ll n){ll ans=1;if(n == 0)return 1;else if(n == 1)return a;else return a*power(a,n-1);}
+//vector<ll>Prime;bool mark[10000009];
+//void sieve(ll n){ll i,j;mark[1]=1;for(i=4; i<=n; i+=2)mark[i]=1;Prime.push_back(2);for(i=3; i<=n; i+=2){if(!mark[i]){Prime.push_back(i);if(i*i<=n){for(j=i*i; j<=n; j+=(i*2))mark[j]=1;}}}}
+//vector<ll>List;
+//void Primefactorize(ll n){for(ll i=0; Prime[i]*Prime[i]<=n; i++){if(n%Prime[i] == 0){while(n%Prime[i] == 0){List.push_back(Prime[i]);n/=Prime[i];}}}if(n>1){List.push_back(n);}}
+/** Order Set **/
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+//template<typename T> using orderset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+//orderset<int> s ; //orderset<int>::iterator it ;
+//orderset<int> X; //X.insert(1); //X.insert(2); //X.insert(4); //X.insert(8); //X.insert(16);
+//cout<<*X.find_by_order(1)<<endl; // 2 //cout<<*X.find_by_order(2)<<endl; // 4 //cout<<*X.find_by_order(4)<<endl; // 16 //cout<<(end(X)==X.find_by_order(6))<<endl; // true
+//cout<<X.order_of_key(-5)<<endl;  // 0 //cout<<X.order_of_key(1)<<endl;   // 0 //cout<<X.order_of_key(3)<<endl;   // 2 //cout<<X.order_of_key(4)<<endl;   // 2 //cout<<X.order_of_key(400)<<endl; // 5
+
+int par[55];
+void makeset(int n)
+{
+    for(int i=1; i<=n; i++)
+        par[i]=i;
+}
+int Find(int node)
+{
+    if(node == par[node])
+        return node;
+    else
+        return par[node]=Find(par[node]);
+}
+struct edge
+{
+    int u,v,w;
+    bool operator < (const edge& p)const
+    {
+        return w<p.w;
+    }
+};
+vector<edge>e;
+bool possible=0;
+int MST(int n)
+{
+    possible=0;
+    sort(e.begin(),e.end());
+    makeset(n);
+    int cnt=0,tot=0;
+    for(int i=0; i<e.size(); i++)
+    {
+        int u=Find(e[i].u);
+        int v=Find(e[i].v);
+        if(u != v)
+        {
+            par[u]=v;
+            cnt+=1;
+            tot += e[i].w;
+            if(cnt == n-1)
+            {
+                possible=1;
+                break;
+            }
+        }
+    }
+    return tot;
+}
+int main()
+{
+//    Output;
+    int i,j,k,n,t,T,ans,tot,need,u,v,w,temp;
+    scin(T);
+    RUN_CASE(t,T)
+    {
+        e.clear();
+        ms(par,0);
+        tot=0;
+        scin(n);
+        for(i=1 ; i<=n ; i++)
+        {
+            edge x;
+            x.u=i;
+            for(j=1 ; j<=n ; j++)
+            {
+                scin(temp);
+                if(i!=j && temp>0)
+                {
+                    x.v=j;
+                    x.w=temp;
+                    e.push_back(x);
+                }
+                tot+=temp;
+            }
+        }
+        need=MST(n);
+        ans=tot-need;
+        if(possible || n==1)
+            pf("Case %d: %d\n",t,ans);
+        else
+            pf("Case %d: -1\n",t);
+    }
+    return 0;
+}
+
