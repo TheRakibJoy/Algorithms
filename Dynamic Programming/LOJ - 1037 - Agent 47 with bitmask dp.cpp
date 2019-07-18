964fe69f6e6ac716a335bfcb531da61814cfaecf
Md. Iqbal Hossain <32066256+Bappy38@users.noreply.github.com>
2019-07-18 16:48:30 +0600

    Add files via upload

---
+++ Dynamic Programming/LOJ - 1037 - Agent 47 with bitmask dp.cpp
@@ -0,0 +1,140 @@
+#include<bits/stdc++.h>
+
+#define Input                   freopen("in.txt","r",stdin)
+#define Output                  freopen("out.txt","w",stdout)
+#define ll                      long long int
+#define ull                     unsigned long long int
+#define pii                     pair<int,int>
+#define pll                     pair<ll,ll>
+#define sc                      scanf
+#define scin(x)                 sc("%d",&(x))
+#define scin2(x,y)              sc("%d %d",&(x),&(y))
+#define scln(x)                 sc("%lld",&(x))
+#define scln2(x,y)              sc("%lld %lld",&(x),&(y))
+#define pf                      printf
+#define all(a)                  (a.begin()),(a.end())
+#define UNIQUE(X)               (X).erase(unique(all(X)),(X).end())
+#define SORT_UNIQUE(c)          (sort(c.begin(),c.end()), c.resize(distance(c.begin(),unique(c.begin(),c.end()))))
+#define ms(a,b)                 memset(a,b,sizeof(a))
+#define pb(a)                   push_back(a)
+#define mp                      make_pair
+#define db                      double
+#define EPS                     10E-10
+#define ff                      first
+#define ss                      second
+#define sqr(x)                  (x)*(x)
+#define vi                      vector<int>
+#define vl                      vector<ll>
+#define vii                     vector<vector<int> >
+#define vll                     vector<vector<ll> >
+#define DBG                     pf("HI\n")
+#define MOD                     1000000007
+#define CIN                     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define RUN_CASE(t,T)           for(__typeof(t) t=1;t<=T;t++)
+#define CASE(t)                 printf("Case %d: ",t)
+#define CASEl(t)                printf("Case %d:\n",t)
+#define intlimit                2147483690
+#define longlimit               92233720368547758
+#define infinity                (1<<28)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                ((a)*(b))/gcd(a,b)
+#define mxx                     123456789
+#define PI                      2*acos(0.0)
+#define rep(i,a,b)              for(__typeof(i) i=a;i<=b;i++)
+#define rev(i,a,b)              for(__typeof(i) i=a;i>=b;i--)
+
+using namespace std;
+/**     toint, tostring, BigMod, Power , sieve, Primefactorize ,frequency in n!     **/
+//ll toint(string s){ll n=0,k=1;for(int i=s.size()-1; i>=0; i--){n += ((s[i]-'0')*k);k*=10;}return n;}
+//string tostring(ll x){string s="";while(x){s += (x%10)+'0';x/=10;}reverse(s.begin(),s.end());return s;}
+//vector<ll>Prime;
+//bool mark[10000003];
+//void sieve(ll n){ll i,j;mark[1]=1;for(i=4; i<=n; i+=2)mark[i]=1;Prime.push_back(2);for(i=3; i<=n; i+=2){if(!mark[i]){Prime.push_back(i);if(i*i<=n){for(j=i*i; j<=n; j+=(i*2))mark[j]=1;}}}}
+//map<ll,ll>Factor;
+//void Primefactorize(ll n){for(ll i=0; i<Prime.size() && Prime[i]*Prime[i]<=n; i++){if(n%Prime[i] == 0){while(n%Prime[i] == 0){Factor[Prime[i]]++;n/=Prime[i];}}}if(n>1){Factor[n]++;}}
+//ll frequency(ll n,ll factor)/** Frequency of a factor in n! **/{ll cnt=0;while(n){cnt += (n/factor);n /= factor;}return cnt;}
+/**     Order Set       **/
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+//template<typename T> using orderset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+//orderset<int> s ; //orderset<int>::iterator it ;
+//orderset<int> X; //X.insert(1); //X.insert(2); //X.insert(4); //X.insert(8); //X.insert(16);
+//cout<<*X.find_by_order(0)<<endl; // 2 //cout<<*X.find_by_order(2)<<endl; // 4 //cout<<*X.find_by_order(4)<<endl; // 16 //cout<<(end(X)==X.find_by_order(6))<<endl; // true
+//cout<<X.order_of_key(-5)<<endl;  // 0 //cout<<X.order_of_key(1)<<endl;   // 0 //cout<<X.order_of_key(3)<<endl;   // 2 //cout<<X.order_of_key(4)<<endl;   // 2 //cout<<X.order_of_key(400)<<endl; // 5
+///------------------Graph Moves-------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///King's move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///Knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///Knight's move
+
+int Set(int N,int pos)
+{
+    return N=N | (1<<pos);
+}
+int reset(int N,int pos)
+{
+    return N= N & ~(1<<pos);
+}
+bool check(int N,int pos)
+{
+    return (bool)(N & (1<<pos));
+}
+
+int health[16],n;
+char w[16][16];
+ll dp[1<<16+2];
+ll FuN(int mask)
+{
+    if(mask == (1<<n)-1)
+        return 0;
+    if(dp[mask] != -1)
+        return dp[mask];
+    ll mn=longlimit;
+    for(int i=0 ; i<n ; i++)
+    {
+        if(check(mask,i) == 0)  /** If ith enemy is not killed yet **/
+        {
+            ll ret,tot_shot;
+            ret = health[i];    /** Initially ret=health[i] ; bcoz if we haven't killed any enemy yet which weapon can
+            be used in ith enemy ; then,we've to killed ith enemy with default weapon which take one shoot for one unit
+            health that means total shoot need to kill ith enemy is equal to health[i] **/
+            for(int j=0 ; j<n ; j++)
+            {
+                /** In problem, The jth digit of the ith line denotes the damage done to
+                 target j, if you use the weapon of target i in each shot **/
+                int digit = w[j][i]-'0';    /** Damage done to target i per shoot,here we use the weapon of
+                target j **/
+                if(digit!=0 && i!=j && check(mask,j)==1)    /** We can use the weapon of target j only when it already killed **/
+                {
+                    tot_shot = (health[i]+digit-1)/digit;   /** Calculate Total needed shoot for killing target i by the weapon of target j **/
+                    ret = min(ret,tot_shot);    /** Minimum needed shot for killing target i **/
+                }
+            }
+            ret = ret+FuN(Set(mask,i));
+            mn=min(mn,ret);
+        }
+    }
+    return dp[mask]=mn;
+}
+int main()
+{
+//    Output;
+    int i,j,k,t,T;
+    ll ans;
+    scin(T);
+    RUN_CASE(t,T)
+    {
+        scin(n);
+        for(i=0 ; i<n ; i++)
+            scin(health[i]);
+        for(i=0 ; i<n ; i++)
+            sc("%s",w[i]);
+        ms(dp,-1);
+        ans = FuN(0);
+        pf("Case %d: %lld\n",t,ans);
+    }
+    return 0;
+}
+
