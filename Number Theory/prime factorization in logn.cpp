96c2b57b2097461027de8739622466f2bbe67452
Md. Iqbal Hossain <32066256+Bappy38@users.noreply.github.com>
2019-09-27 16:40:29 +0600

    Update prime factorization in logn.cpp

--- Number Theory/prime factorization in logn.cpp
+++ Number Theory/prime factorization in logn.cpp
@@ -1,35 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool mark[1000005];
-int SP[1000005];        /** Here we store smallest factor for each number **/
-vector<int> Prime;
-void Sieve(int n)
+int SF[1000005];        /** Here we store smallest factor for each number **/
+void FuN()
 {
-    long long int i,j;
-    mark[1]=1;
-    for(i=4 ; i<=n ; i+=2){
-        mark[i]=1;
-        SP[i]=2;
-    }
-    Prime.push_back(2);
-    SP[1]=1;
-    SP[2]=2;
-    for(i=3 ; i<=n ; i+=2)
-    {
-        if(!mark[i])
-        {
-            Prime.push_back(i);
-            SP[i]=i;
-            if(i*i <= n)    /** Can be overflow **/
-            {
-                for(j=i*i ; j<=n ; j+=(i*2)){
-                    mark[j]=1;
-                    if(!SP[j])SP[j]=i;
-                }
+    for( int i = 2 ; i * i <= 10000000 ; i ++ ){
+        if( !SF[i] ){
+            for( int j = i ; j <= 10000000 ; j += i ){
+                if( !SF[j] ) SF[j] = i ;
             }
         }
     }
+    for( int i = 2 ; i <= 1e7 ; i ++ ) if( !SF[i] ) SF[i] = i ;
 }
 vector<int>Factor;
 void getFactor(int n)       /** It will works when prime till n is generated **/
@@ -37,14 +19,14 @@ void getFactor(int n)       /** It will works when prime till n is generated **/
     Factor.clear();
     while(n != 1)
     {
-        Factor.push_back(SP[n]);
-        n/=SP[n];
+        Factor.push_back(SF[n]);
+        n/=SF[n];
     }
 }
 int main()
 {
     int i,j,n;
-    Sieve(100000);
+    FuN(1e7);
     getFactor(45);
     for(i=0 ; i<Factor.size() ; i++)
     {
