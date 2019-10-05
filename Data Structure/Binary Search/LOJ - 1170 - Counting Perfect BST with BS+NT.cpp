ff8a2b77d648a7c0ac497a125eae00d420924e5b
Md. Iqbal Hossain <32066256+Bappy38@users.noreply.github.com>
2019-10-06 01:39:45 +0600

    Add files via upload

---
+++ Data Structure/Binary Search/LOJ - 1170 - Counting Perfect BST with BS+NT.cpp
@@ -0,0 +1,146 @@
+#include<bits/stdc++.h>
+
+#define Input                   freopen("in.txt","r",stdin)
+#define Output                  freopen("out.txt","w",stdout)
+#define ll                      long long int
+#define ull                     unsigned long long int
+#define pii                     pair<int,int>
+#define pll                     pair<ll,ll>
+#define sc                      scanf
+#define scin(x)                 sc("%d",&(x))
+#define scin2(x,y)              sc("%d %d",&(x),&(y))
+#define scln(x)                 sc("%lld",&(x))
+#define scln2(x,y)              sc("%lld %lld",&(x),&(y))
+#define pf                      printf
+#define all(a)                  (a.begin()),(a.end())
+#define UNIQUE(X)               (X).erase(unique(all(X)),(X).end())
+#define SORT_UNIQUE(c)          (sort(c.begin(),c.end()), c.resize(distance(c.begin(),unique(c.begin(),c.end()))))
+#define ms(a,b)                 memset(a,b,sizeof(a))
+#define pb(a)                   push_back(a)
+#define mp                      make_pair
+#define db                      double
+#define EPS                     10E-10
+#define ff                      first
+#define ss                      second
+#define sqr(x)                  (x)*(x)
+#define vi                      vector<int>
+#define vl                      vector<ll>
+#define vii                     vector<vector<int> >
+#define vll                     vector<vector<ll> >
+#define DBG                     pf("HI\n")
+#define MOD                     100000007
+#define CIN                     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define RUN_CASE(t,T)           for(__typeof(t) t=1;t<=T;t++)
+#define CASE(t)                 printf("Case %d: ",t)
+#define CASEl(t)                printf("Case %d:\n",t)
+#define intlimit                2147483690
+#define longlimit               92233720368547758
+#define infinity                (1<<28)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                ((a)*(b))/gcd(a,b)
+#define mxx                     123456789
+#define PI                      2*acos(0.0)
+#define rep(i,a,b)              for(__typeof(i) i=a;i<=b;i++)
+#define rev(i,a,b)              for(__typeof(i) i=a;i>=b;i--)
+
+using namespace std;
+/**     toint, tostring, BigMod, Power , sieve, Primefactorize ,frequency in n!     **/
+//ll toint(string s){ll n=0,k=1;for(int i=s.size()-1; i>=0; i--){n += ((s[i]-'0')*k);k*=10;}return n;}
+//string tostring(ll x){string s="";while(x){s += (x%10)+'0';x/=10;}reverse(s.begin(),s.end());return s;}
+//ll BigMod(ll a,ll b){if(b == 0)return 1%MOD;else if(b%2 == 0){ll temp=BigMod(a,b/2);return ((temp%MOD)*(temp%MOD))%MOD;}else{return ((a%MOD)*BigMod(a,b-1)%MOD)%MOD;}}
+//vector<ll>Prime;
+//bool mark[10000003];
+//void sieve(ll n){ll i,j;mark[1]=1;for(i=4; i<=n; i+=2)mark[i]=1;Prime.push_back(2);for(i=3; i<=n; i+=2){if(!mark[i]){Prime.push_back(i);if(i*i<=n){for(j=i*i; j<=n; j+=(i*2))mark[j]=1;}}}}
+//map<ll,ll>Factor;
+//void Primefactorize(ll n){for(ll i=0; i<Prime.size() && Prime[i]*Prime[i]<=n; i++){if(n%Prime[i] == 0){while(n%Prime[i] == 0){Factor[Prime[i]]++;n/=Prime[i];}}}if(n>1){Factor[n]++;}}
+//ll frequency(ll n,ll factor)/** Frequency of a factor in n! **/{ll cnt=0;while(n){cnt += (n/factor);n /= factor;}return cnt;}
+/**     Order Set       **/
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+//template<typename T> using orderset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+//orderset<int> s ; //orderset<int>::iterator it ;
+//orderset<int> X; //X.insert(1); //X.insert(2); //X.insert(4); //X.insert(8); //X.insert(16);
+//cout<<*X.find_by_order(0)<<endl; // 2 //cout<<*X.find_by_order(2)<<endl; // 4 //cout<<*X.find_by_order(4)<<endl; // 16 //cout<<(end(X)==X.find_by_order(6))<<endl; // true
+//cout<<X.order_of_key(-5)<<endl;  // 0 //cout<<X.order_of_key(1)<<endl;   // 0 //cout<<X.order_of_key(3)<<endl;   // 2 //cout<<X.order_of_key(4)<<endl;   // 2 //cout<<X.order_of_key(400)<<endl; // 5
+///------------------Graph Moves-------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///King's move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///Knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///Knight's move
+
+#define sz 1000005
+ll fact[sz],catalan[sz];
+void Factorial()
+{
+    fact[0] = 1;
+    fact[1] = 1;
+    for(ll i=2 ; i<=1e6 ; i++)
+        fact[i] = (i*fact[i-1])%MOD;
+}
+ll BigMOD(ll a,ll n)
+{
+    if(n == 0LL)
+        return 1LL%MOD;
+    else if(n%2 == 0)
+    {
+        ll temp = BigMOD(a, n/2)%MOD;
+        return ((temp%MOD) * (temp%MOD))%MOD;
+    }
+    else
+        return ((a%MOD) * (BigMOD(a,n-1)%MOD))%MOD;
+}
+ll nCr(ll n,ll r)
+{
+    ll upper = fact[n];
+    ll lower = (fact[r]*fact[n-r])%MOD;
+    ll ret = ((upper%MOD) * BigMOD(lower,MOD-2))%MOD;
+    return ret;
+}
+void Cat()
+{
+    catalan[0] = 0;
+    for(ll i=1 ; i<=2e5 ; i++)
+    {
+        ll ans = (fact[i+1]*fact[i])%MOD;
+        ans = BigMOD(ans , MOD-2);
+        ans = (fact[2*i] * ans)%MOD;
+        catalan[i] = ans;
+    }
+}
+vector<ll>PerfectPower;
+void PerFect()
+{
+    for(ll i=2 ; i<=100001 ; i++)
+    {
+        ll x=i*i;
+        while(x <= 1e10){
+            PerfectPower.pb(x);
+            x *= i;
+        }
+    }
+    PerfectPower.pb(1e10);
+    sort(PerfectPower.begin() , PerfectPower.end());
+    UNIQUE(PerfectPower);
+}
+int main()
+{
+//    Output;
+    Factorial();
+    PerFect();
+    Cat();
+    ll i,j,k,a,b,ans,t,T,x,y,n;
+    scln(T);
+    RUN_CASE(t,T)
+    {
+        scln2(a,b);
+        x = lower_bound(PerfectPower.begin() , PerfectPower.end() , a)-PerfectPower.begin();
+        y = upper_bound(PerfectPower.begin() , PerfectPower.end() , b)-PerfectPower.begin();
+        n = y-x;
+        ans = catalan[n];
+        pf("Case %lld: %lld\n",t,ans);
+    }
+    return 0;
+}
+
