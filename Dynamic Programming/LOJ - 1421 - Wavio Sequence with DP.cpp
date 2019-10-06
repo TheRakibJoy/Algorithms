f98391693fa3ccd56fb9178b4395e440b5e779a9
Md. Iqbal Hossain <32066256+Bappy38@users.noreply.github.com>
2019-10-07 02:11:48 +0600

    Add files via upload

---
+++ Dynamic Programming/LOJ - 1421 - Wavio Sequence with DP.cpp
@@ -0,0 +1,140 @@
+#include<bits/stdc++.h>
+
+#define Input                   freopen("in.txt","r",stdin)
+#define Output                  freopen("out.txt","w",stdout)
+#define ll                      long long int
+#define ull                     unsigned long long int
+#define pii                     pair<int,int>
+#define pll                     pair<ll,ll>
+#define sc                      scanf
+#define scin(x)                 sc("%d",&(x))
+#define scin2(x,y)              sc("%d %d",&(x),&(y))
+#define scln(x)                 sc("%lld",&(x))
+#define scln2(x,y)              sc("%lld %lld",&(x),&(y))
+#define pf                      printf
+#define all(a)                  (a.begin()),(a.end())
+#define UNIQUE(X)               (X).erase(unique(all(X)),(X).end())
+#define SORT_UNIQUE(c)          (sort(c.begin(),c.end()), c.resize(distance(c.begin(),unique(c.begin(),c.end()))))
+#define ms(a,b)                 memset(a,b,sizeof(a))
+#define pb(a)                   push_back(a)
+#define mp                      make_pair
+#define db                      double
+#define EPS                     10E-10
+#define ff                      first
+#define ss                      second
+#define sqr(x)                  (x)*(x)
+#define vi                      vector<int>
+#define vl                      vector<ll>
+#define vii                     vector<vector<int> >
+#define vll                     vector<vector<ll> >
+#define DBG                     pf("HI\n")
+#define MOD                     1000000007
+#define CIN                     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define RUN_CASE(t,T)           for(__typeof(t) t=1;t<=T;t++)
+#define CASE(t)                 printf("Case %d: ",t)
+#define CASEl(t)                printf("Case %d:\n",t)
+#define intlimit                2147483690
+#define longlimit               92233720368547758
+#define infinity                (1<<28)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                ((a)*(b))/gcd(a,b)
+#define mxx                     123456789
+#define PI                      2*acos(0.0)
+#define rep(i,a,b)              for(__typeof(i) i=a;i<=b;i++)
+#define rev(i,a,b)              for(__typeof(i) i=a;i>=b;i--)
+
+using namespace std;
+/**     toint, tostring, BigMod, Power , sieve, Primefactorize ,frequency in n!     **/
+//ll toint(string s){ll n=0,k=1;for(int i=s.size()-1; i>=0; i--){n += ((s[i]-'0')*k);k*=10;}return n;}
+//string tostring(ll x){string s="";while(x){s += (x%10)+'0';x/=10;}reverse(s.begin(),s.end());return s;}
+//ll BigMod(ll a,ll b){if(b == 0)return 1%MOD;else if(b%2 == 0){ll temp=BigMod(a,b/2);return ((temp%MOD)*(temp%MOD))%MOD;}else{return ((a%MOD)*BigMod(a,b-1)%MOD)%MOD;}}
+//ll Power(ll a,ll n){ll ret=1;for(ll i=1 ; i<=n ; i++)ret = ((ret%MOD)*(a%MOD))%MOD;return ret;}
+//vector<ll>Prime;
+//bool mark[10000003];
+//void sieve(ll n){ll i,j;mark[1]=1;for(i=4; i<=n; i+=2)mark[i]=1;Prime.push_back(2);for(i=3; i<=n; i+=2){if(!mark[i]){Prime.push_back(i);if(i*i<=n){for(j=i*i; j<=n; j+=(i*2))mark[j]=1;}}}}
+//map<ll,ll>Factor;
+//void Primefactorize(ll n){for(ll i=0; i<Prime.size() && Prime[i]*Prime[i]<=n; i++){if(n%Prime[i] == 0){while(n%Prime[i] == 0){Factor[Prime[i]]++;n/=Prime[i];}}}if(n>1){Factor[n]++;}}
+//ll frequency(ll n,ll factor)/** Frequency of a factor in n! **/{ll cnt=0;while(n){cnt += (n/factor);n /= factor;}return cnt;}
+/**     Order Set       **/
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+//template<typename T> using orderset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+//orderset<int> s ; //orderset<int>::iterator it ;
+//orderset<int> X; //X.insert(1); //X.insert(2); //X.insert(4); //X.insert(8); //X.insert(16);
+//cout<<*X.find_by_order(0)<<endl; // 2 //cout<<*X.find_by_order(2)<<endl; // 4 //cout<<*X.find_by_order(4)<<endl; // 16 //cout<<(end(X)==X.find_by_order(6))<<endl; // true
+//cout<<X.order_of_key(-5)<<endl;  // 0 //cout<<X.order_of_key(1)<<endl;   // 0 //cout<<X.order_of_key(3)<<endl;   // 2 //cout<<X.order_of_key(4)<<endl;   // 2 //cout<<X.order_of_key(400)<<endl; // 5
+///------------------Graph Moves-------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///King's move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///Knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///Knight's move
+
+/** Problem:
+    Wavio is a sequence of integers. It has some interesting properties:
+    1. Wavio is of odd length i.e. L = 2*n + 1.
+    2. The first (n+1) integers of Wavio sequence make a strictly increasing sequence.
+    3. The last (n+1) integers of Wavio sequence make a strictly decreasing sequence.
+    4. No two adjacent integers are same in a Wavio sequence.
+    Find the maximum possible length of wavio sequence
+**/
+#define sz 100005
+int n,L[2][sz],I[sz];
+vi ara;
+/**  Here, L[i] means from 0 to i we can build L[i] length LIS   **/
+void LISNlogK(int state)
+{
+    int i;
+    I[0] = -infinity;
+    for(i=1 ; i<=n ; i++){
+        I[i] = infinity;
+        L[state][i] = n;
+    }
+    int LisLen = 0;
+    for(i=0 ; i<n ; i++){
+        int lo=0,hi=LisLen,mid;
+        while(lo <= hi){
+            mid = (lo+hi)/2;
+            if(ara[i] > I[mid])
+                lo = mid+1;
+            else
+                hi = mid-1;
+        }
+        I[lo] = ara[i];
+        L[state][i] = lo;   /** Store LIS from starting to ith position **/
+        if(LisLen < lo)
+            LisLen = lo;    /** Store overall LIS **/
+    }
+}
+void Solve(int t)
+{
+    ara.clear();
+    int i,j,ans=0,temp;
+    scin(n);
+    rep(i,0,n-1)
+    {
+        scin(temp);
+        ara.pb(temp);
+    }
+    LISNlogK(0);
+    reverse(ara.begin() , ara.end());
+    LISNlogK(1);    /** Reverse LIS means LDS -_- **/
+    for(i=0,j=n-1 ; i<n ; i++,j--){ /** Here,L[0][i] means LIS from 0 to i ; L[0][j] means LIS from n-1 to j
+            or LDS from j to n-1 **/
+    /** From every position we've to take LIS from starting & LDS till ending of maximum possible same length **/
+        int mn = min(L[0][i] , L[1][j]);
+        ans = max(ans , (2*mn)-1);
+    }
+    pf("Case %d: %d\n",t,ans);
+}
+int main()
+{
+    int t,T;
+    scin(T);
+    RUN_CASE(t,T){
+        Solve(t);
+    }
+    return 0;
+}
+
