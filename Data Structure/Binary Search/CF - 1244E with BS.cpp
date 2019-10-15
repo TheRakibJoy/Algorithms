506d520b590bfb300b35394926e2eb6dfc8f80b2
Md. Iqbal Hossain <32066256+Bappy38@users.noreply.github.com>
2019-10-16 00:37:11 +0600

    Add files via upload

---
+++ Data Structure/Binary Search/CF - 1244E with BS.cpp
@@ -0,0 +1,145 @@
+#include<bits/stdc++.h>
+
+#define Input                   freopen("in.txt","r",stdin)
+#define Output                  freopen("out.txt","w",stdout)
+#define ll                      long long int
+#define ull                     unsigned long long int
+#define pii                     pair<int,int>
+#define pll                     pair<ll,ll>
+#define sc                      scanf
+#define scin(x)                 sc("%d",&(x))
+#define scin2(x,y)              sc("%d %d",&(x),&(y))
+#define scln(x)                 sc("%lld",&(x))
+#define scln2(x,y)              sc("%lld %lld",&(x),&(y))
+#define pf                      printf
+#define all(a)                  (a.begin()),(a.end())
+#define UNIQUE(X)               (X).erase(unique(all(X)),(X).end())
+#define SORT_UNIQUE(c)          (sort(c.begin(),c.end()), c.resize(distance(c.begin(),unique(c.begin(),c.end()))))
+#define ms(a,b)                 memset(a,b,sizeof(a))
+#define pb(a)                   push_back(a)
+#define mp                      make_pair
+#define db                      double
+#define EPS                     10E-10
+#define ff                      first
+#define ss                      second
+#define sqr(x)                  (x)*(x)
+#define vi                      vector<int>
+#define vl                      vector<ll>
+#define vii                     vector<vector<int> >
+#define vll                     vector<vector<ll> >
+#define DBG                     pf("HI\n")
+#define MOD                     1000000007
+#define CIN                     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define RUN_CASE(t,T)           for(__typeof(t) t=1;t<=T;t++)
+#define CASE(t)                 printf("Case %d: ",t)
+#define CASEl(t)                printf("Case %d:\n",t)
+#define intlimit                2147483690
+#define longlimit               92233720368547758
+#define infinity                (1<<28)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                ((a)*(b))/gcd(a,b)
+#define mxx                     123456789
+#define PI                      2*acos(0.0)
+#define rep(i,a,b)              for(__typeof(i) i=a;i<=b;i++)
+#define rev(i,a,b)              for(__typeof(i) i=a;i>=b;i--)
+
+using namespace std;
+/**     toint, tostring, BigMod, Power , sieve, Primefactorize ,frequency in n!     **/
+//ll toint(string s){ll n=0,k=1;for(int i=s.size()-1; i>=0; i--){n += ((s[i]-'0')*k);k*=10;}return n;}
+//string tostring(ll x){string s="";while(x){s += (x%10)+'0';x/=10;}reverse(s.begin(),s.end());return s;}
+//ll BigMod(ll a,ll b){if(b == 0)return 1%MOD;else if(b%2 == 0){ll temp=BigMod(a,b/2);return ((temp%MOD)*(temp%MOD))%MOD;}else{return ((a%MOD)*BigMod(a,b-1)%MOD)%MOD;}}
+//ll Power(ll a,ll n){ll ret=1;for(ll i=1 ; i<=n ; i++)ret = ((ret%MOD)*(a%MOD))%MOD;return ret;}
+//vector<ll>Prime;
+//bool mark[10000003];
+//void sieve(ll n){ll i,j;mark[1]=1;for(i=4; i<=n; i+=2)mark[i]=1;Prime.push_back(2);for(i=3; i<=n; i+=2){if(!mark[i]){Prime.push_back(i);if(i*i<=n){for(j=i*i; j<=n; j+=(i*2))mark[j]=1;}}}}
+//map<ll,ll>Factor;
+//void Primefactorize(ll n){for(ll i=0; i<Prime.size() && Prime[i]*Prime[i]<=n; i++){if(n%Prime[i] == 0){while(n%Prime[i] == 0){Factor[Prime[i]]++;n/=Prime[i];}}}if(n>1){Factor[n]++;}}
+//ll frequency(ll n,ll factor)/** Frequency of a factor in n! **/{ll cnt=0;while(n){cnt += (n/factor);n /= factor;}return cnt;}
+/**     Order Set       **/
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+//template<typename T> using orderset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+//orderset<int> s ; //orderset<int>::iterator it ;
+//orderset<int> X; //X.insert(1); //X.insert(2); //X.insert(4); //X.insert(8); //X.insert(16);
+//cout<<*X.find_by_order(0)<<endl; // 2 //cout<<*X.find_by_order(2)<<endl; // 4 //cout<<*X.find_by_order(4)<<endl; // 16 //cout<<(end(X)==X.find_by_order(6))<<endl; // true
+//cout<<X.order_of_key(-5)<<endl;  // 0 //cout<<X.order_of_key(1)<<endl;   // 0 //cout<<X.order_of_key(3)<<endl;   // 2 //cout<<X.order_of_key(4)<<endl;   // 2 //cout<<X.order_of_key(400)<<endl; // 5
+///------------------Graph Moves-------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///King's move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///Knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///Knight's move
+
+/**
+Problem:
+You are given a sequence a1,a2,…,an consisting of n integers.
+You may perform the following operation on this sequence: choose any element and either
+increase or decrease it by one.
+Calculate the minimum possible difference between the maximum element and the minimum
+element in the sequence, if you can perform the aforementioned operation no more than k
+times.
+**/
+ll n,k,ara[100005],sum[100005];
+ll Valid(ll dif)
+{
+    ll tot=1e17;
+    for(ll i=1 ; i<=n ; i++){
+
+        ll cur1 = ara[i];       /// Assumed minimum value of the array
+        ll cur2 = ara[i]+dif;   /// Assumed maximum value of the array
+
+        ll pos2 = upper_bound(ara+1 , ara+n+1 , cur2)-ara;
+
+        ll incrementCost = ((i-1)*ara[i]) - sum[i-1];
+        /** Cost for make all the elements equal to cur1 those who r less than cur1 **/
+        ll decrementCost = (sum[n]-sum[pos2-1]) - ((n-pos2+1)*cur2);
+        /** Cost for make all the elements equal to cur2 those who r greater than cur2 **/
+
+        tot = min(tot , incrementCost+decrementCost);
+    }
+    for(ll i=1 ; i<=n ; i++){
+
+        ll cur1 = ara[i]-dif;
+        ll cur2 = ara[i];
+
+        ll pos1 = lower_bound(ara+1 , ara+n+1 , cur1)-ara;
+        pos1--;
+
+        ll incrementCost = (pos1*cur1) - sum[pos1];
+        /** Cost for make all the elements equal to cur1 those who r less than cur1 **/
+        ll decrementCost = (sum[n]-sum[i]) - ((n-i)*cur2);
+        /** Cost for make all the elements equal to cur1 those who r greater than cur2 **/
+
+        tot = min(tot , incrementCost+decrementCost);
+    }
+    return tot;
+}
+ll BiSec()
+{
+    /** Assume a difference & check it is valid or not? **/
+    ll lo=0,hi=ara[n]-ara[1],mid,res=ara[n]-ara[1];
+    while(lo <= hi){
+        mid = (lo+hi)/2;
+        if(Valid(mid) <= k){
+            res = mid;
+            hi = mid-1;
+        }
+        else
+            lo = mid+1;
+    }
+    return res;
+}
+int main()
+{
+    ll i,j,ans;
+    scln2(n,k);
+    rep(i,1,n)scln(ara[i]);
+    sort(ara+1 , ara+n+1);
+    for(i=1 ; i<=n ; i++)
+        sum[i] = sum[i-1]+ara[i];
+    ans = BiSec();
+    cout<<ans<<endl;
+    return 0;
+}
+
