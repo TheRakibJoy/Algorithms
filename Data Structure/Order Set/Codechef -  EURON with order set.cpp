9e343ea01365b0bdd8d27e0d20316bc3e48dfec8
Md. Iqbal Hossain <32066256+Bappy38@users.noreply.github.com>
2020-06-04 00:51:40 +0600

    Add files via upload

---
+++ Data Structure/Order Set/Codechef -  EURON with order set.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+
+#define Input                   freopen("in.txt","r",stdin)
+#define Output                  freopen("out.txt","w",stdout)
+#define ll                      long long int
+#define ull                     unsigned long long int
+#define pii                     pair<int,int>
+#define pll                     pair<ll,ll>
+#define sc                      scanf
+#define scin(x)                 sc("%d",&(x))
+#define scin2(x,y)              sc("%d %d",&(x),&(y))
+#define scln(x)                 sc("%lld",&(x))
+#define scln2(x,y)              sc("%lld %lld",&(x),&(y))
+#define pf                      printf
+#define all(a)                  (a.begin()),(a.end())
+#define UNIQUE(X)               (X).erase(unique(all(X)),(X).end())
+#define SORT_UNIQUE(c)          (sort(c.begin(),c.end()), c.resize(distance(c.begin(),unique(c.begin(),c.end()))))
+#define ms(a,b)                 memset(a,b,sizeof(a))
+#define pb(a)                   push_back(a)
+#define mp                      make_pair
+#define db                      double
+#define EPS                     10E-10
+#define ff                      first
+#define ss                      second
+#define sqr(x)                  (x)*(x)
+#define vi                      vector<int>
+#define vl                      vector<ll>
+#define vii                     vector<vector<int> >
+#define vll                     vector<vector<ll> >
+#define DBG                     pf("HI\n")
+#define MOD                     1000000007
+#define CIN                     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define RUN_CASE(t,T)           for(__typeof(t) t=1;t<=T;t++)
+#define CASE(t)                 printf("Case %d: ",t)
+#define CASEl(t)                printf("Case %d:\n",t)
+#define intlimit                2147483690
+#define longlimit               9223372036854775800
+#define infinity                (1<<28)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                (a)*(b)/gcd(a,b)
+#define mxx                     123456789
+#define PI                      2*acos(0.0)
+#define rep(i,a,b)              for(__typeof(i) i=a;i<=b;i++)
+#define rev(i,a,b)              for(__typeof(i) i=a;i>=b;i--)
+
+using namespace std;
+
+/**     Order Set       **/
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template<typename T> using orderset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+//orderset<int> s ; //orderset<int>::iterator it ;
+//orderset<int> X; //X.insert(1); //X.insert(2); //X.insert(4); //X.insert(8); //X.insert(16);
+//cout<<*X.find_by_order(0)<<endl; // 2 //cout<<*X.find_by_order(2)<<endl; // 4 //cout<<*X.find_by_order(4)<<endl; // 16 //cout<<(end(X)==X.find_by_order(6))<<endl; // true
+//cout<<X.order_of_key(-5)<<endl;  // 0 //cout<<X.order_of_key(1)<<endl;   // 0 //cout<<X.order_of_key(3)<<endl;   // 2 //cout<<X.order_of_key(4)<<endl;   // 2 //cout<<X.order_of_key(400)<<endl; // 5
+
+orderset<pll>st;    ///First-Value ; Second-Index
+
+void Solve(int t)
+{
+    ll i,j,k,n,ans=0,temp;
+    scln(n);
+    for(i=1 ; i<=n ; i++){
+        scln(temp);
+        ll id = st.order_of_key({temp , i});
+        ll sz = i-1;
+        ans += (sz-id);
+        st.insert(make_pair(temp , i));
+    }
+    cout<<ans<<endl;
+}
+
+int main()
+{
+    int t,T;
+    T = 1;
+//    scin(T);
+    RUN_CASE(t,T)
+    {
+        Solve(t);
+    }
+    return 0;
+}
+
+
