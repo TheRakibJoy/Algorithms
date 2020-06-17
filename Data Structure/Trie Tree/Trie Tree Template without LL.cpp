cac90fc140efeb0dbcfb4371b23959e1ffebfd60
Md. Iqbal Hossain <32066256+Bappy38@users.noreply.github.com>
2020-06-18 01:12:55 +0600

    Add files via upload

---
+++ Data Structure/Trie Tree/Trie Tree Template without LL.cpp
@@ -0,0 +1,108 @@
+#include<bits/stdc++.h>
+
+#define Input                   freopen("in.txt","r",stdin)
+#define Output                  freopen("out.txt","w",stdout)
+#define ll                      long long int
+#define ull                     unsigned long long int
+#define pii                     pair<int,int>
+#define pll                     pair<ll,ll>
+#define sc                      scanf
+#define scin(x)                 sc("%d",&(x))
+#define scin2(x,y)              sc("%d %d",&(x),&(y))
+#define scln(x)                 sc("%lld",&(x))
+#define scln2(x,y)              sc("%lld %lld",&(x),&(y))
+#define pf                      printf
+#define all(a)                  (a.begin()),(a.end())
+#define UNIQUE(X)               (X).erase(unique(all(X)),(X).end())
+#define SORT_UNIQUE(c)          (sort(c.begin(),c.end()), c.resize(distance(c.begin(),unique(c.begin(),c.end()))))
+#define ms(a,b)                 memset(a,b,sizeof(a))
+#define pb(a)                   push_back(a)
+#define mp                      make_pair
+#define db                      double
+#define EPS                     10E-10
+#define ff                      first
+#define ss                      second
+#define sqr(x)                  (x)*(x)
+#define vi                      vector<int>
+#define vl                      vector<ll>
+#define vii                     vector<vector<int> >
+#define vll                     vector<vector<ll> >
+#define DBG                     pf("HI\n")
+#define MOD                     1000000007
+#define CIN                     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define RUN_CASE(t,T)           for(__typeof(t) t=1;t<=T;t++)
+#define CASE(t)                 printf("Case %d: ",t)
+#define CASEl(t)                printf("Case %d:\n",t)
+#define intlimit                2147483690
+#define longlimit               9223372036854775800
+#define infinity                (1<<28)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                (a)*(b)/gcd(a,b)
+#define mxx                     123456789
+#define PI                      2*acos(0.0)
+#define rep(i,a,b)              for(__typeof(i) i=a;i<=b;i++)
+#define rev(i,a,b)              for(__typeof(i) i=a;i>=b;i--)
+
+using namespace std;
+
+
+/** You're given n words & q queries. Search a string that is it exist in the dictionary
+or not? **/
+
+const int N = 1e6+100;
+int tot_node = 1;
+int to[N][26] , endmark[N];
+
+void Add(string &s)
+{
+    int sz = (int)s.size() , curr = 1;   ///curr - Root node
+    for(int i=0 ; i<sz ; i++){
+        int ch = s[i]-'a';
+        if(!to[curr][ch])    to[curr][ch] = ++tot_node;
+        curr = to[curr][ch];
+    }
+    endmark[curr] = 1;
+}
+
+bool Search(string &s)
+{
+    int sz = (int)s.size() , curr = 1;
+    for(int i=0 ; i<sz ; i++){
+        int ch = s[i]-'a';
+        if(!to[curr][ch])   return 0;
+        curr = to[curr][ch];
+    }
+    return endmark[curr];
+}
+
+void Solve(int t)
+{
+    int i,j,k,n,q;
+    string s;
+    scin2(n , q);
+    for(i=1 ; i<=n ; i++){
+        cin>>s;
+        Add(s);
+    }
+    for(i=1 ; i<=q ; i++){
+        cin>>s;
+        if(Search(s))
+            pf("It exist.\n");
+        else
+            pf("Not exist.\n");
+    }
+}
+
+int main()
+{
+    int t,T;
+    T = 1;
+//    scin(T);
+    RUN_CASE(t,T)
+    {
+        Solve(t);
+    }
+    return 0;
+}
+
+
